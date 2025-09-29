efabea2ee17fa2715853e7419dded3449c85da4a
WajidKarimm <karimwajid0101@gmail.com>
2025-09-29 13:09:10 +0500

    new upadtes

--- blog/src/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, login, register, detail   # include detail here
-
-urlpatterns = [
-    path('', home, name="home"),          
-    path('login/', login, name="login"), 
-    path('register/', register, name="register"),
-    path('detail/', detail, name="detail"),
-]
